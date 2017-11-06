d7bcb43b67be988dd957b4d3e6c0399b014edb26
Theresa Shen <tshen@architech.ca>
2017-11-06 15:10:14 -0500

    Promote BookList as a Container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+// Container: a react/smart component that has a direct connection to the state managed by redux
+// Promote BookList from a Component into a Container
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -10,6 +13,7 @@ export default class BookList extends Component {
   }
 
   render() {
+    // console.log(this.props.asdf); // -> '123'
     return (
       <ul className="list-group col-sm-4">
         {this.renderList()}
@@ -17,3 +21,18 @@ export default class BookList extends Component {
     )
   }
 }
+
+// Glue between React and Redux
+// Takes Application state and return props inside the containers
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return {
+    // asdf: '123'
+    books: state.books
+  };
+}
+
+// export the container
+// connect takes a function and a component and produces a container
+export default connect(mapStateToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { BooksReducer } from './reducer_books';
+import BooksReducer from './reducer_books';
 
 // Application State - Genereated by reducers
 // {
