8464395d13d626918d5a94b91ff03a61c9b23c01
Theresa Shen <tshen@architech.ca>
2017-11-06 13:51:53 -0500

    Create BooksReducer to return an array of books

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
+import { BooksReducer } from './reducer_books';
 
+// Application State - Genereated by reducers
+// {
+//   books: [{title: 'Harry Potter'}, {title: 'Javascript'}],
+//   activeBook: {title: 'Javascript: The Good Parts'}
+// }
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// Return an array of books
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' }
+  ]
+}
