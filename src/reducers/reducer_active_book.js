328388992abc00804627dca356a45a13ca917bf1
Theresa Shen <tshen@architech.ca>
2018-04-29 18:33:20 -0400

    Add some comments

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,6 +1,6 @@
 // State argument is not application state, only the state
 // this reducer is responsible for
-export default function(state = null , action) {
+export default function(state = null , action) {  // if the state is undefined, set it to null
   switch (action.type) {
   case 'BOOK_SELECTED':
     return action.payload;
